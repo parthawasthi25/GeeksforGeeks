7e202d99dc8e06d280e54cf68063de580df22fc5
Parth Awasthi <114384177+parthawasthi25@users.noreply.github.com>
2024-07-18 16:16:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap two numbers/swap-two-numbers.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial template for c++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    pair<int, int> get(int a, int b){
+      a=a+b;
+      b=a-b;
+      a=a-b;
+      return make_pair(a,b);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        
+        Solution ob;
+        pair<int, int>p = ob.get(a, b);
+        cout << p.first << ' ' << p.second << endl;
+    
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
