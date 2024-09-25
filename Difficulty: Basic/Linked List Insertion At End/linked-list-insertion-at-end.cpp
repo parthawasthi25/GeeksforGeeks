020f305c1c0f5415f83450e8584af78ac5c7db0f
Parth Awasthi <114384177+parthawasthi25@users.noreply.github.com>
2024-09-25 22:31:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        Node * temp = new Node(x);
+        if(head == NULL)
+            return temp;
+        Node* current = head;
+        while(current -> next != NULL){
+            current = current -> next;
+        }
+        current -> next = temp;
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+
+        Solution ob;
+        Node* ans = ob.insertAtEnd(head, x);
+        printList(ans);
+    }
+
+    return 0;
+}
+// } Driver Code Ends
